510f2a038251f04717eaa9adda391a58414e19ad
easywony <justwon1201@gmail.com>
2026-04-28 14:53:13 +0900

    feat: Todo 데이터 모델 추가

---
+++ todo/Models/Todo.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+struct Todo: Identifiable, Equatable, Codable {
+    let id: UUID
+    var title: String
+    var isCompleted: Bool
+    var dueDate: Date?
+    var priority: Priority
+
+    enum Priority: String, CaseIterable, Equatable, Codable {
+        case high, medium, low
+    }
+
+    init(
+        id: UUID = UUID(),
+        title: String,
+        isCompleted: Bool = false,
+        dueDate: Date? = nil,
+        priority: Priority = .medium
+    ) {
+        self.id = id
+        self.title = title
+        self.isCompleted = isCompleted
+        self.dueDate = dueDate
+        self.priority = priority
+    }
+}
