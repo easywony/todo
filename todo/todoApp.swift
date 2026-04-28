a2a94d11d97b196377e5218451d4ffdd3e20348a
easywony <justwon1201@gmail.com>
2026-04-28 11:59:12 +0900

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by GSITM on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
