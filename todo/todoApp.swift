c1f86ab83d48b4f6e74b84238b6728ef6fbba7b7
easywony <justwon1201@gmail.com>
2026-04-29 10:19:17 +0900

    feat: TodoListView를 앱 루트 화면으로 연결

--- todo/todoApp.swift
+++ todo/todoApp.swift
@@ -1,17 +1,15 @@
-//
-//  todoApp.swift
-//  todo
-//
-//  Created by GSITM on 4/28/26.
-//
-
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct todoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TodoListView(
+                store: Store(initialState: TodoListFeature.State()) {
+                    TodoListFeature()
+                }
+            )
         }
     }
 }
