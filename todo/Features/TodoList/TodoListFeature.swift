5fd15db527fb1c31ac94ccb6f68aceba068d5198
easywony <justwon1201@gmail.com>
2026-04-29 10:45:26 +0900

    fix: body 프로퍼티 제거하고 reduce 메서드로 circular reference 해결

--- todo/Features/TodoList/TodoListFeature.swift
+++ todo/Features/TodoList/TodoListFeature.swift
@@ -20,44 +20,42 @@ struct TodoListFeature {
 
     @Dependency(\.todoRepository) var todoRepository
 
-    var body: some ReducerOf<Self> {
-        Reduce { state, action in
-            switch action {
-            case .onAppear:
-                state.isLoading = true
-                return .run { send in
-                    do {
-                        let todos = try await todoRepository.fetchAll()
-                        await send(.todosLoaded(todos))
-                    } catch {
-                        await send(.todoLoadFailed)
-                    }
+    func reduce(into state: inout State, action: Action) -> Effect<Action> {
+        switch action {
+        case .onAppear:
+            state.isLoading = true
+            return .run { send in
+                do {
+                    let todos = try await todoRepository.fetchAll()
+                    await send(.todosLoaded(todos))
+                } catch {
+                    await send(.todoLoadFailed)
                 }
-
-            case .todosLoaded(let todos):
-                state.isLoading = false
-                state.todos = IdentifiedArray(uniqueElements: todos)
-                return .none
-
-            case .todoLoadFailed:
-                state.isLoading = false
-                return .none
-
-            case .toggleCompleted(let id):
-                state.todos[id: id]?.isCompleted.toggle()
-                return .run { [todos = state.todos] _ in
-                    try await todoRepository.save(Array(todos))
-                }
-
-            case .deleteTodo(let id):
-                state.todos.remove(id: id)
-                return .run { [todos = state.todos] _ in
-                    try await todoRepository.save(Array(todos))
-                }
-
-            case .addTodoButtonTapped, .todoRowTapped:
-                return .none
             }
+
+        case .todosLoaded(let todos):
+            state.isLoading = false
+            state.todos = IdentifiedArray(uniqueElements: todos)
+            return .none
+
+        case .todoLoadFailed:
+            state.isLoading = false
+            return .none
+
+        case .toggleCompleted(let id):
+            state.todos[id: id]?.isCompleted.toggle()
+            return .run { [todos = state.todos] _ in
+                try await todoRepository.save(Array(todos))
+            }
+
+        case .deleteTodo(let id):
+            state.todos.remove(id: id)
+            return .run { [todos = state.todos] _ in
+                try await todoRepository.save(Array(todos))
+            }
+
+        case .addTodoButtonTapped, .todoRowTapped:
+            return .none
         }
     }
 }
